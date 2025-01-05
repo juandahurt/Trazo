619d25e1b6797112a7a30927cb313311ac8445e0
Juan David Hurtado <juandahurtadom@gmail.com>
2025-01-05 11:09:38 -0500

    Add substraction

--- TrazoTests/Core/VectorTests.swift
+++ TrazoTests/Core/VectorTests.swift
@@ -14,7 +14,7 @@ struct VectorTests {
         (Vector(x: 81, y: 2), Vector(x: 2, y:-14), false),
         (Vector(x: 1, y: 1), Vector(x: 1, y: 1), true),
     ])
-    func testEquality(a: Vector, b: Vector, actuallyEqual: Bool) {
+    func equality(a: Vector, b: Vector, actuallyEqual: Bool) {
         let areEqual = a == b
         #expect(areEqual == actuallyEqual)
     }
@@ -23,8 +23,17 @@ struct VectorTests {
         (Vector(x: 1, y: 10), Vector(x: 3, y: 2), Vector(x: 4, y: 12)),
         (Vector(x: -3, y: 1), Vector(x: -3, y: 10), Vector(x: -6, y: 11)),
     ])
-    func testSum(a: Vector, b: Vector, actualResult: Vector) {
+    func sum(a: Vector, b: Vector, actualResult: Vector) {
         let sum = a + b
+        #expect(sum == actualResult)
+    }
+    
+    @Test("Vector sum", arguments: [
+        (Vector(x: 1, y: 10), Vector(x: 3, y: 2), Vector(x: -2, y: 8)),
+        (Vector(x: -3, y: 1), Vector(x: -3, y: 10), Vector(x: 0, y: -9)),
+    ])
+    func substraction(a: Vector, b: Vector, actualResult: Vector) {
+        let sum = a - b
         #expect(sum == actualResult)
     }
 }
