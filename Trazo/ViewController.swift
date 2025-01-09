a290a39725abc7bc62aef26141b5dfc721239f71
Juan David Hurtado <juandahurtadom@gmail.com>
2025-01-09 13:50:41 -0500

    Add grid view

--- Trazo/ViewController.swift
+++ Trazo/ViewController.swift
@@ -7,7 +7,54 @@
 
 import UIKit
 
+class GridView: UIView {
+    
+    override func didMoveToSuperview() {
+        backgroundColor = .init(red: 32 / 255, green: 32 / 255, blue: 32 / 255, alpha: 1)
+    }
+    
+    override func draw(_ rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+        
+        drawGrid(using: context)
+    }
+    
+    func drawGrid(using context: CGContext) {
+        let viewWidth = bounds.width
+        let viewHeight = bounds.height
+        let cellSize: CGFloat = 10
+        
+        let numCols = Int(viewWidth / cellSize)
+        for col in 0...numCols {
+            let x = CGFloat(col) * cellSize
+            context.move(to: .init(x: x, y: 0))
+            context.setStrokeColor(red: 37 / 255, green: 37 / 255, blue: 37 / 255, alpha: 1)
+            context.setLineWidth(1)
+            context.addLine(to: .init(x: x, y: viewHeight))
+        }
+        
+        let numRows = Int(viewHeight / cellSize)
+        for row in 0...numRows {
+            let y = CGFloat(row) * cellSize
+            context.move(to: .init(x: 0, y: y))
+            context.setStrokeColor(red: 37 / 255, green: 37 / 255, blue: 37 / 255, alpha: 1)
+            context.setLineWidth(1)
+            context.addLine(to: .init(x: viewWidth, y: y))
+        }
+        
+        context.strokePath()
+    }
+}
+
 class ViewController: UIViewController {
+    private lazy var gridView: GridView = {
+        let gridView = GridView()
+        gridView.translatesAutoresizingMaskIntoConstraints = false
+        return gridView
+    }()
+    
     private lazy var canvasView: CanvasView = {
         let canvasView = CanvasView()
         canvasView.translatesAutoresizingMaskIntoConstraints = false
@@ -17,10 +64,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .gray
-        
         addPinchGesture()
-        setupCanvasView()
+        addSubviews()
+    }
+    
+    func addSubviews() {
+        addGridView()
+        addCanvasView()
+    }
+    
+    func addGridView() {
+        view.addSubview(gridView)
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: gridView.topAnchor),
+            view.bottomAnchor.constraint(equalTo: gridView.bottomAnchor),
+            view.leadingAnchor.constraint(equalTo: gridView.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: gridView.trailingAnchor),
+        ])
     }
     
     func addPinchGesture() {
@@ -31,7 +91,7 @@ class ViewController: UIViewController {
         view.addGestureRecognizer(pinchRecognizer)
     }
     
-    func setupCanvasView() {
+    func addCanvasView() {
         view.addSubview(canvasView)
         
         NSLayoutConstraint.activate(
