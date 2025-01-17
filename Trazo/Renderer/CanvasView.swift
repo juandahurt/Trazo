d6475efd3c6a8f016434f5a2d10130a3a461a503
Juan David Hurtado <juandahurtadom@gmail.com>
2025-01-17 18:16:18 -0500

    Add canvas manager

--- Trazo/Renderer/CanvasView.swift
+++ Trazo/Renderer/CanvasView.swift
@@ -7,6 +7,88 @@
 
 import MetalKit
 
+class CanvasManager {
+    var renderer: Renderer
+    var brushSize: Float = 10
+    var currentBrush: Brush
+    var lastAddedPoint: Point?
+    var contentScaleFactor: Float = 1
+    let desiredDistance: Float = 3
+    weak var canvasView: CanvasView?
+    
+    init() {
+        renderer = Renderer()
+        currentBrush = .init(textureName: "default")
+    }
+    
+    func setup(with canvasView: CanvasView) {
+        canvasView.delegate = renderer
+        self.canvasView = canvasView
+        contentScaleFactor = Float(canvasView.contentScaleFactor)
+    }
+    
+    private func location(of touch: UITouch) -> Vector {
+        let cgPoint = touch.location(in: canvasView)
+        return .init(
+            x: Float(cgPoint.x) * contentScaleFactor,
+            y: Float(cgPoint.y) * contentScaleFactor
+        )
+    }
+    
+    func computePointScale(givenAForceOf force: Float) -> Float {
+        guard force != 0 else { return brushSize }
+        return max(1, brushSize * force)
+    }
+    
+    func touchBegan(touch: UITouch) {
+        let location = location(of: touch)
+        let point = Point(
+            scale: computePointScale(givenAForceOf: Float(touch.force)),
+            position: location
+        )
+        renderer.addLine(.init(points: [point]))
+        lastAddedPoint = point
+    }
+    
+    func touchMoved(touch: UITouch) {
+        guard var lastAddedPoint else { return }
+        let location = location(of: touch)
+        var point = Point(
+            scale: computePointScale(givenAForceOf: Float(touch.force)),
+            position: location
+        )
+        
+        let dist = distance(point.position, lastAddedPoint.position)
+        let dir = normalize(point.position - lastAddedPoint.position)
+        if dist > desiredDistance {
+            // get the number of points that can fit in the space
+            let numOfPoints = Int(dist / desiredDistance)
+            // add more points in the space
+            var lastScale = lastAddedPoint.scale
+            let scaleDiff = point.scale - lastScale
+            let step = scaleDiff / Float(numOfPoints)
+            
+            for i in 0...numOfPoints {
+                guard numOfPoints > 2 else { break }
+                let newPointPos = lastAddedPoint.position + (
+                    dir * desiredDistance * Float(i)
+                )
+                let newPoint = Point(
+                    scale: lastScale + (step * Float(i)),
+                    position: newPointPos
+                )
+                renderer.addPoint(newPoint)
+            }
+        }
+        renderer.addPoint(point)
+        self.lastAddedPoint = point
+    }
+    
+    func touchEnded() {
+        lastAddedPoint = nil
+    }
+}
+
 struct Brush {
     var textureName: String
     var texture: MTLTexture?
@@ -33,9 +115,7 @@ struct Line {
 }
 
 class CanvasView: MTKView {
-    let renderer = Renderer()
-    let desiredDistance: Float = 2
-    let brushSize: Float = 10.0
+    let manager = CanvasManager()
     
     init() {
         guard let device = MTLCreateSystemDefaultDevice() else {
@@ -45,7 +125,7 @@ class CanvasView: MTKView {
         
         colorPixelFormat = RendererSettings.pixelFormat
         clearColor = .init(red: 1, green: 1, blue: 1, alpha: 1)
-        delegate = renderer
+        manager.setup(with: self)
     }
 
     required init(coder: NSCoder) {
@@ -54,75 +134,21 @@ class CanvasView: MTKView {
 }
 
 extension CanvasView {
-    func computePointScale(givenAForceOf force: Float) -> Float {
-        guard force != 0 else { return brushSize }
-        return brushSize * force
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // TODO: check if I need to do something if user uses more than one finger
+        guard touches.count == 1 else { return }
         guard let touch = touches.first else { return }
-        let cgPoint = touch.location(in: self)
-        renderer.addLine(.init(points: [
-            .init(
-                scale: computePointScale(givenAForceOf: Float(touch.force)),
-                position: [
-                    Float(cgPoint.x) * Float(contentScaleFactor),
-                    Float(cgPoint.y) * Float(contentScaleFactor)
-                ]
-            )
-        ]))
+        manager.touchBegan(touch: touch)
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard touches.count == 1 else { return }
-        guard let lastPoint = renderer.lines[renderer.lines.count - 1].points.last else {
-            return
-        }
         guard let touch = touches.first else { return }
-
-        let cgPoint = touch.location(in: self)
-        var point = Point(
-            scale: computePointScale(givenAForceOf: Float(touch.force)),
-            position: [
-                Float(cgPoint.x) * Float(contentScaleFactor),
-                Float(cgPoint.y) * Float(contentScaleFactor)
-            ]
-        )
-        
-        let dist = distance(point.position, lastPoint.position)
-        let dir = normalize(point.position - lastPoint.position)
-        if dist > desiredDistance {
-            // get the number of points that can fit in the space
-            let numOfPoints = Int(dist / desiredDistance)
-            // add more points in the space
-            var lastAddedPoint: simd_float2?
-            let lastScale = lastPoint.scale
-            let scaleDiff = point.scale - lastScale
-            let step = scaleDiff / Float(numOfPoints)
-            
-            for i in 0...numOfPoints {
-                let newPointPos = lastPoint.position + (dir * desiredDistance * Float(i))
-                addPointToLine(
-                    .init(
-                        scale: lastScale + (step * Float(i)),
-                        position: newPointPos
-                    )
-                )
-                lastAddedPoint = newPointPos
-            }
-            if let lastAddedPoint {
-                point.position = lastAddedPoint + dir * desiredDistance
-                addPointToLine(point)
-            }
-        } else if dist < desiredDistance {
-            point.position = lastPoint.position + dir * desiredDistance
-            addPointToLine(point)
-        }
+        manager.touchMoved(touch: touch)
     }
     
-    func addPointToLine(_ point: Point) {
-//        renderer.lines[renderer.lines.count - 1].points.append(point)
-        renderer.addPoint(point)
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard touches.count == 1 else { return }
+        guard let _ = touches.first else { return }
+        manager.touchEnded()
     }
 }
