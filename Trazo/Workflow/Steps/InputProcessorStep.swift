6afc45617e63fbb9895ed55bd81d4cf9f60346c3
Juan David Hurtado <juandahurtadom@gmail.com>
2025-02-13 11:31:13 -0500

    Fix touch location

--- Trazo/Workflow/Steps/InputProcessorStep.swift
+++ Trazo/Workflow/Steps/InputProcessorStep.swift
@@ -10,12 +10,14 @@ import UIKit
 class InputProcessorStep: WorkflowStep {
     override func excecute(using state: inout CanvasState) {
         print("executing iput processor step")
-        let center = state.canvasView.center
-        let location = state.inputTouch.location(in: state.canvasView)
-        let scaledX = (location.x - center.x) / CGFloat(state.currScale) + center.x
-        let scaledY = (location.y - center.y) / CGFloat(state.currScale) + center.y
-        let x = (scaledX / state.canvasView.bounds.width) * 2 - 1
-        let y = 1 - (scaledY / state.canvasView.bounds.height) * 2
-        state.drawableTouch.positionInTextCoord = .init(x: x, y: y)
+        var location = state.inputTouch.location(in: state.canvasView)
+        
+        // convert to metal coords
+        let x = (location.x / state.canvasView.bounds.width) * 2 - 1
+        let y = 1 - (location.y / state.canvasView.bounds.height) * 2
+        
+        location = CGPoint(x: x, y: y).applying(state.ctm.inverted())
+        
+        state.drawableTouch.positionInTextCoord = .init(x: location.x, y: location.y)
     }
 }
--- Trazo/Workflow/Steps/TransformCanvasStep.swift
+++ Trazo/Workflow/Steps/TransformCanvasStep.swift
@@ -10,13 +10,11 @@ import CoreGraphics
 class TransformCanvasStep: WorkflowStep {
     override func excecute(using state: inout CanvasState) {
         print("transform canvas step")
-        
-        state.ctm = state.ctm.scaledBy(x: state.scale, y: state.scale)
-        state.ctm = state.ctm.rotated(by: 2 * .pi - state.rotation)
-//        state.canvasView.transform = state.canvasView.transform.scaledBy(
-//            x: state.transformScale,
-//            y: state.transformScale
-//        )
+       
+        state.ctm = state.ctm
+            .rotated(by: 2 * .pi - state.rotation)
+            .scaledBy(x: state.scale, y: state.scale)
+            
         // make sure the background is cleared on the presetation step
         state.canvasBackgroundColor = (0.125, 0.125, 0.125, 1)
     }
