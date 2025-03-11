ed565024bc68de1138d169274e413818b86c2b97
Juan David Hurtado <juandahurtadom@gmail.com>
2025-03-11 11:54:22 -0500

    Add custom vector type

--- Trazo/Workflow/Steps/CurveFittingStep.swift
+++ Trazo/Workflow/Steps/CurveFittingStep.swift
@@ -5,24 +5,30 @@
 //  Created by Juan Hurtado on 23/02/25.
 //
 
-import CoreGraphics
-import simd
+import TrazoCore
 
 struct Segment {
-    var a, b, c, d: CGPoint
-    var p1p2Dist: CGFloat
+    var a, b, c, d: vector_t
+    var p1p2Dist: Float
 }
 
 class CurveFittingStep: WorkflowStep {
-    func calculateSegment(p0: CGPoint, p1: CGPoint, p2: CGPoint, p3: CGPoint, alpha: CGFloat, tension: CGFloat) -> Segment {
-        let t0: CGFloat = 0.0
-        let t1 = t0 + pow(p0.distance(to: p1), alpha)
-        let t2 = t1 + pow(p1.distance(to: p2), alpha)
-        let t3 = t2 + pow(p2.distance(to: p3), alpha)
+    func calculateSegment(
+        p0: vector_t,
+        p1: vector_t,
+        p2: vector_t,
+        p3: vector_t,
+        alpha: Float,
+        tension: Float
+    ) -> Segment {
+        let t0: Float = 0.0
+        let t1 = t0 + pow(p0.distance_to(p1), alpha)
+        let t2 = t1 + pow(p1.distance_to(p2), alpha)
+        let t3 = t2 + pow(p2.distance_to(p3), alpha)
         
         let m1 = (1 - tension) * (t2 - t1) *
         ((p1 - p0) / (t1 - t0) - (p2 - p0) / (t2 - t0) + (p2 - p1) / (t2 - t1))
-        
+
         let m2 = (1 - tension) * (t2 - t1) *
         ((p2 - p1) / (t2 - t1) - (p3 - p1) / (t3 - t1) + (p3 - p2) / (t3 - t2))
         
@@ -31,15 +37,15 @@ class CurveFittingStep: WorkflowStep {
             b: -3 * (p1 - p2) - m1 - m1 - m2,
             c: m1,
             d: p1,
-            p1p2Dist: p1.distance(to: p2)
+            p1p2Dist: p1.distance_to(p2)
         )
     }
     
     func addPointsFor(segment: Segment, state: inout CanvasState) {
         let steps = Int(segment.p1p2Dist)
         for i in 0..<steps {
-            let t = CGFloat(i) / CGFloat(steps)
-            let newPoint: CGPoint = segment.a * pow(t, 3) +
+            let t = Float(i) / Float(steps)
+            let newPoint: vector_t = segment.a * pow(t, 3) +
             segment.b * pow(t, 2) +
             segment.c * t +
             segment.d
@@ -57,8 +63,8 @@ class CurveFittingStep: WorkflowStep {
         let p2 = state.currentCurve.points[i + 1]
         let p3 = state.currentCurve.points[i + 2]
         
-        let alpha = 0.5
-        let tension = 0.0
+        let alpha: Float = 0.5
+        let tension: Float = 0.0
         
         let segment = calculateSegment(
             p0: p0,
---
+++ TrazoCore/Sources/TrazoCore/include/TrazoCore.h
@@ -0,0 +1,8 @@
+//
+//  TrazoCore.h
+//  TrazoCore
+//
+//  Created by Juan Hurtado on 11/03/25.
+//
+
+#include "vector_t.hpp"
