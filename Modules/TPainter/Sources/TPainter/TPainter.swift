4e6b6d003607f73e69d197470de0299c6d5af82d
juandahurt <juandahurtadom@gmail.com>
2025-06-15 12:03:18 -0500

    fix point generation

--- Modules/TPainter/Sources/TPainter/TPainter.swift
+++ Modules/TPainter/Sources/TPainter/TPainter.swift
@@ -40,47 +40,96 @@ public struct TPainter {
         
         switch touch.phase {
         case .moved:
-            guard touchCount > 3 else { return }
-            
-            let index = touchCount - 3
-            let (c1, c2) = findControlPoints(
-                p0: stroke[index-1].location,
-                p1: stroke[index].location,
-                p2: stroke[index+1].location,
-                p3: stroke[index+2].location
-            )
-            let p0 = stroke[index].location
-            let p1 = c1
-            let p2 = c2
-            let p3 = stroke[index+1].location
-            
-            let length = distance(p0, p1) + distance(p1, p2) + distance(p2, p3)
-            let density: Float = 1
-            let n = Int(length * density)
-            
-            var points: [TGRenderablePoint] = []
-            for index in 0..<n {
-                let t = Float(index) / Float(n)
-                var location = cubicBezierValue(
-                    p0: p0,
-                    p1: p1,
-                    p2: p2,
-                    p3: p3,
-                    t: t
-                )
-                if brush.jitter > 0 {
-                    location.x += Float.random(in: -brush.jitter..<brush.jitter)
-                    location.y += Float.random(in: -brush.jitter..<brush.jitter)
-                }
-                points.append(.init(location: location, size: brushSize))
+            guard touchCount >= 3 else { return }
+            if touchCount == 3 {
+                points.append(contentsOf: findPointsForFirstSegment())
+            } else {
+                points.append(contentsOf: findPointsForMidSegment())
             }
-            self.points.append(contentsOf: points)
-        case .began, .ended, .cancelled:
-            // TODO: draw last part of the stroke
-            points.append(.init(location: touch.location, size: brushSize))
-            break
+        case .ended, .cancelled:
+            guard touchCount > 3 else { return }
+            // add the second-last curve
+            points.append(contentsOf: findPointsForMidSegment())
+            // add the last curve
+            points.append(contentsOf: findPointsForFinalSegment())
         default: break
         }
+    }
+    
+    func findPointsForFirstSegment() -> [TGRenderablePoint] {
+        let index = 0
+        let (c1, c2) = findControlPoints(
+            p0: stroke[0].location,
+            p1: stroke[0].location,
+            p2: stroke[1].location,
+            p3: stroke[2].location
+        )
+        let p0 = stroke[index].location
+        let p1 = c1
+        let p2 = c2
+        let p3 = stroke[1].location
+        return findPointsForSegment(p0: p0, p1: p1, p2: p2, p3: p3)
+    }
+    
+    func findPointsForFinalSegment() -> [TGRenderablePoint] {
+        let index = touchCount - 1
+        let (c1, c2) = findControlPoints(
+            p0: stroke[index-1].location,
+            p1: stroke[index].location,
+            p2: stroke[index].location,
+            p3: stroke[index].location
+        )
+        let p0 = stroke[index-1].location
+        let p1 = c1
+        let p2 = c2
+        let p3 = stroke[index].location
+        return findPointsForSegment(p0: p0, p1: p1, p2: p2, p3: p3)
+    }
+    
+    func findPointsForMidSegment() -> [TGRenderablePoint] {
+        let index = touchCount - 3
+        let (c1, c2) = findControlPoints(
+            p0: stroke[index-1].location,
+            p1: stroke[index].location,
+            p2: stroke[index+1].location,
+            p3: stroke[index+2].location
+        )
+        let p0 = stroke[index].location
+        let p1 = c1
+        let p2 = c2
+        let p3 = stroke[index+1].location
+        
+        return findPointsForSegment(p0: p0, p1: p1, p2: p2, p3: p3)
+    }
+    
+    func findPointsForSegment(
+        p0: simd_float2,
+        p1: simd_float2,
+        p2: simd_float2,
+        p3: simd_float2
+    ) -> [TGRenderablePoint] {
+        let length = distance(p0, p1) + distance(p1, p2) + distance(p2, p3)
+        let density: Float = 1
+        let n = Int(length * density)
+        
+        var points: [TGRenderablePoint] = []
+        for index in 0..<n {
+            let t = Float(index) / Float(n)
+            var location = cubicBezierValue(
+                p0: p0,
+                p1: p1,
+                p2: p2,
+                p3: p3,
+                t: t
+            )
+            if brush.jitter > 0 {
+                location.x += Float.random(in: -brush.jitter..<brush.jitter)
+                location.y += Float.random(in: -brush.jitter..<brush.jitter)
+            }
+            points.append(.init(location: location, size: brushSize))
+        }
+        
+        return points
     }
     
     func findControlPoints(
