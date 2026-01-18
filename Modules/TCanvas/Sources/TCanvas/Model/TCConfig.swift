c5c482ce282ef434eca520243e62ad9884fbb7e4
Juan David Hurtado <juandahurtadom@gmail.com>
2026-01-17 20:10:01 -0500

    remove old code

--- Modules/TCanvas/Sources/TCanvas/Model/TCConfig.swift
+++
@@ -1,9 +0,0 @@
-public struct TCConfig {
-    let isTransformEnabled: Bool
-    let brush: TCBrush
-    
-    public init(isTransformEnabled: Bool, brush: TCBrush) {
-        self.isTransformEnabled = isTransformEnabled
-        self.brush = brush
-    }
-}
